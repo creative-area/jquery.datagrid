cf2ac939ece7ef725448ebb7e91af2f6b0ed0583
Balda <xavier@creative-area.net>
2014-01-08 00:03:18 +0100

    demo one page

--- demo/data/demo-simple.php
+++ demo/data/demo-simple.php
@@ -8,13 +8,66 @@ if ( $paging <= 0 ) $paging = 20;
 $orderby = $_POST['orderby'];
 $direction = $_POST['direction'];
 
+// filters
+$aFilters = array();
+$sql_filters = '';
+
+$Continent = ( isset($_POST['Continent']) ) ? $_POST['Continent'] : false;
+$ContinentAllowed = array( 
+	"Asia",
+	"Europe",
+	"North America",
+	"Africa",
+	"Oceania",
+	"Antarctica",
+	"South America"
+);
+if ( !empty($Continent) && in_array($Continent, $ContinentAllowed) ) {
+	$aFilters[] = ' Continent = "'.$Continent.'" ';
+}
+$PopulationInterval = ( isset($_POST['PopulationInterval']) ) ? $_POST['PopulationInterval'] : false;
+if ( !empty($PopulationInterval) ) {
+	switch( $PopulationInterval ) {
+		case 1:
+			$aFilters[] = ' Population < 1000000 ';
+		break;
+		case 2:
+			$aFilters[] = ' Population >= 1000000 AND Population < 100000000 ';
+		break;
+		case 3:
+			$aFilters[] = ' Population >= 100000000 ';
+		break;
+	}
+}
+$SurfaceArea = ( isset($_POST['SurfaceArea']) ) ? $_POST['SurfaceArea'] : false;
+$SurfaceArea = ( $SurfaceArea != "null" ) ? $SurfaceArea : "";
+if ( !empty($SurfaceArea) ) {
+	$aTemp = array();
+	if ( in_array(1, $SurfaceArea) ) {
+		$aTemp[] = ' SurfaceArea >= 1000000 ';
+	}
+	if ( in_array(2, $SurfaceArea) ) {
+		$aTemp[] = ' SurfaceArea >= 100000 AND SurfaceArea < 1000000 ';
+	}
+	if ( in_array(3, $SurfaceArea) ) {
+		$aTemp[] = ' SurfaceArea < 100000 ';
+	}
+	if ( sizeof( $aTemp ) > 0 ) {
+		$aFilters[] = ' ( '.implode(' OR ', $aTemp ).' ) ';
+	}
+}
+
+if ( sizeof($aFilters) > 0 ) {
+	$sql_filters = ' WHERE '.implode(' AND', $aFilters);
+}
+
 $result = array(
 	'data' => array(),
 	'total' => 0
 );
 
 // total
-$sql_all = 'SELECT COUNT(*) FROM Country';
+$sql_all = 'SELECT COUNT(*) FROM Country'.$sql_filters;
 $result_total = mysql_query($sql_all);
 $row_total = mysql_fetch_array($result_total);
 $result['total'] = $row_total[0];
@@ -23,7 +76,8 @@ $result['total'] = $row_total[0];
 $sql = '
 	SELECT *
 	FROM Country
-	'.( ( !empty($orderby) ) ? 'ORDER BY '.$orderby.' '.$direction.'' : '' ).'
+	'.$sql_filters.'
+	'.( ( !empty($orderby) ) ? 'ORDER BY '.mysql_real_escape_string($orderby).' '.mysql_real_escape_string($direction).'' : '' ).'
 	LIMIT '.( ($page-1)*$paging ).', '.$paging.'
 ';
 $data = mysql_query($sql);
